9016d717ef2dbe62084cba582619d221af89ba3f
Miguel D <70726341+U0447@users.noreply.github.com>
2026-03-20 18:51:16 +0100

    Update export.sh

--- export.sh
+++ export.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
+
+TOTAL=$(cat "/opt/backups/zmigrate/emails.txt"| wc -l)
+COUNTER=1
+
 while read -r i; do
 echo "$i"
 
+echo $COUNTER"/"$TOTAL" : "$I
 out="$(z mprov -l ga $i |grep -Ei '(userPassword|Name:|zimbraAccountStatus:|zimbraPrefMailForwardingAddress:)')"
 
 awk '/^userPassword: / {print $2}' <<< "$out" > "userpass/$i.shadow"
 awk '!/^userPassword: /' <<< "$out" > "userdata/$i.txt"
 
+((COUNTER++))
 done < emails.txt
+
+
