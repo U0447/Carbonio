563fe40ad8bce9138ebbd8e70b58eee81cfeb407
Miguel D <70726341+U0447@users.noreply.github.com>
2026-03-30 11:13:22 +0200

    Update exportData.sh

--- exportData.sh
+++ exportData.sh
@@ -5,7 +5,7 @@ total=$(cat emails.txt | wc -l)
 while read -r i; do
 echo $contador"/"$total": "$i
 
-do zmmailbox -z -m $i -t 0 getRestURL '/?fmt=tgz' > /opt/backup/migrate/MBOX&/$i.tgz
+zmmailbox -z -m $i -t 0 getRestURL '/?fmt=tgz' > /opt/backup/migrate/MBOX&/$i.tgz
 
 ((contador++))
 done < emails.txt
