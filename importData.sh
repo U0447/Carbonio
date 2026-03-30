7047f2d27336932571554a00e9283f5a1e04e81a
Miguel D <70726341+U0447@users.noreply.github.com>
2026-03-30 11:13:34 +0200

    Update importData.sh

--- importData.sh
+++ importData.sh
@@ -5,7 +5,7 @@ total=$(cat emails.txt | wc -l)
 while read -r i; do
 echo $contador"/"$total": "$i
 
-do zmmailbox -z -m $i -t 0 postRestURL "/?fmt=tgz&resolve=skip" /opt/backups/zmigrate/MBOX/$i.tgz
+zmmailbox -z -m $i -t 0 postRestURL "/?fmt=tgz&resolve=skip" /opt/backups/zmigrate/MBOX/$i.tgz
 
 ((contador++))
 done < emails.txt
